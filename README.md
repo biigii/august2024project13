592fb017ad05c8b649fcefdf1eb8c9dbaffdced2
Behshad <82580731+biigii@users.noreply.github.com>
2024-09-17 21:02:01 +1200

    flexbox

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Project 13
 ---
 - [CodePen JS1](https://codepen.io/behshad/pen/wmdeqO)
+- [flex basic](https://codepen.io/behshad/pen/PojPavK)
--- scripts.js
+++
@@ -1,9 +0,0 @@
-// var mainHeading = document.createElement("h1")
-// // mainHeading.innerHTML = "hello world";
-// mainHeading.textContent = "hello world";
-// document.body.appendChild(mainHeading)
-
-var p = document.createElement("p");
-p.textContent = "some text from javascript";
-var content = document.querySelector("article.content");
-content.appendChild(p) ;
